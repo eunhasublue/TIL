2f9e696afb1a6c3686fe246cc87df86063128c11
eunhasublue <eunhasublooming@gmail.com>
2021-12-12 16:37:04 +0900

    Study: Bundler Webpack

---
+++ webpack/webpack-template-basic/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  // 파일을 읽어들이기 시작하는 진입점 설정
+  entry: "./js/main.js",
+
+  // 결과물(번들)을 반환하는 설정
+  output: {
+    // path: path.resolve(__dirname, "dist"),
+    // filename: "main.js",
+    clean: true, // 새롭게 빌드를 돌렸을 때, 필료없는 것을 삭제
+  },
+
+  module: {
+    rules: [
+      {
+        test: /./,
+        use: ["style-loader", "css-loader", "sass-loder"],
+      },
+    ],
+  },
+
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "static" }],
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+  },
+};
